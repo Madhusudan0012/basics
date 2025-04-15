5508051191ffe56f7b58624ff80c63d91ed3a35f
Madhusudan <madhusudanchauhan62@gmail.com>
2025-04-15 11:52:34 +0530

    learn about tokens

--- src/models/User_Model.js
+++ src/models/User_Model.js
@@ -51,5 +51,27 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save" , async function(next){
+    if(!this.isModified("passsword")) return next();
+    this.password = bcrypt.hash(this.password) 
+    next();
+}) 
+
+//for checking password 
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password , this.password)
+}
+
+
+userSchema.methods.genrateAccessToken = function(){}
+userSchema.methods.genrateRefreshToken = function(){}
+
+//both methods used for generating tokens
+
+
+
+
+
+
 
 export const User = mongoose.model("User" , userSchema);
